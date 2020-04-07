22b3d45e3b15dabd2f6266cbb3d67a24d496b28b
Brenda-M <brendanjuguna1@gmail.com>
2020-04-07 23:09:48 +0300

    add logic to find any user on github

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -11,6 +11,7 @@ import { Repository } from '../models/repository'
 export class ProfileService {
 
   userProfile: User;
+  userName: string;
   reposList: any;
   userRepos: Repository;
   myUserName: string;
@@ -19,13 +20,14 @@ export class ProfileService {
   constructor(private http: HttpClient) { 
     this.userProfile = new User ('', '', '', '', 0, '', '', new Date(), 0, 0);
     this.userRepos = new Repository ('', '', '');
+    this.myUserName = 'Brenda-M';
   }
 
   getMyData(){
     interface ApiResponse{
       login: string;
       avatar_url: string;
-      html_url: string
+      html_url: string;
       bio: string;
       public_repos: number;
       hireable: string;
@@ -36,7 +38,7 @@ export class ProfileService {
   }
 
   const promise = new Promise((resolve) => {
-    this.myUserName = 'Brenda-M';
+   
     this.http.get<ApiResponse>(this.apiUrl + this.myUserName + '?access_token=' + environment.accessToken).toPromise().then(response => {
         this.userProfile.login = response.login;
         this.userProfile.html_url = response.html_url;
@@ -55,6 +57,10 @@ export class ProfileService {
     return promise;
   }
 
+  updateProfile(userName){
+    this.myUserName = userName;
+  }
+
   getMyRepos(){
     interface ApiRepositoryResponse{
       name: string;
@@ -63,11 +69,11 @@ export class ProfileService {
     }
 
     const repoPromise = new Promise((resolve, reject) => {
-      this.http.get<ApiRepositoryResponse>(this.apiUrl+ this.myUserName + '/repos?order=created&sort=asc?access_token=' + environment.accessToken).toPromise().then(RepoResponse => {
+      this.http.get<ApiRepositoryResponse>(this.apiUrl+ this.myUserName + '/repos?access_token=' + environment.accessToken).toPromise().then(RepoResponse => {
         this.reposList = RepoResponse;
         resolve();
-      })
-    })
+      });
+    });
 
 
   }
