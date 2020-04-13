285600514da67e9f6108951abe9ab4de5c9e0aec
Brenda-M <brendanjuguna1@gmail.com>
2020-04-13 17:42:03 +0300

    add appHighlight selector to html

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ProfileService } from '../../services/profile.service';
+import {  GithubReposService } from '../../services/github-repos.service';
 import { User } from 'src/app/models/user';
+import { Repository } from 'src/app/models/repository';
 
 @Component({
   selector: 'app-profile',
@@ -11,12 +13,29 @@ import { User } from 'src/app/models/user';
 export class ProfileComponent implements OnInit {
 
   userProfile: User;
+  userName: string;
+  repoName: string;
+  foundRepos: any;
+  repoSearchName: string;
+  showUserForm: boolean = true;
+  showRepoForm: boolean = false;
+  repoList: Repository;
 
-  constructor(public userService: ProfileService) { }
+  constructor(public userService: ProfileService, public repoService: ProfileService, public searchReposService:  GithubReposService) { }
+
+  findUser(value){
+    this.userService.updateProfile(this.userName);
+    this.ngOnInit();
+  }
+
+  findRepo(value){
+    this.searchReposService.searchRepos(this.repoSearchName);
+  }
 
   ngOnInit() {
     this.userService.getUserInfo();
     this.userProfile = this.userService.userProfile;
+    this.repoService.getRepos(); 
   }
 
 }
--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef) { }
 
+  @HostListener('mouseenter') onMouserEnter(){
+    this.highlight('teal');
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.highlight(null);
+
+  }
+  
+  private highlight(color: string) {
+    this.element.nativeElement.style.backgroundColor = color;
+  }
+  
 }
