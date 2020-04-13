5235227241cb84788e76eed43ef1bade9dbe3f2e
Brenda-M <brendanjuguna1@gmail.com>
2020-04-14 01:26:16 +0300

    style repository page

---
+++ src/app/components/repositories/repositories.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubReposService } from '../../services/github-repos.service';
+import { Repository } from '../../models/repository';
+
+@Component({
+  selector: 'app-repositories',
+  templateUrl: './repositories.component.html',
+  styleUrls: ['./repositories.component.css']
+})
+export class RepositoriesComponent implements OnInit {
+
+  repos: Repository;
+  view: number;
+  repoSearchName: string;
+
+  constructor(public searchReposService:  GithubReposService) { 
+    this.repos = new Repository('', '', '', '', '', 0, 0, new Date());
+  }
+
+  
+  findRepo(value){
+    this.searchReposService.searchRepos(this.repoSearchName);
+  }
+
+  // loadMore(){
+  //   this.searchReposService.updateView(this.view);
+  //   this.searchReposService. searchRepos(this.repoSearchName).subscribe(repos => {
+  //    this.repos = repos;
+  //  };
+
+  ngOnInit(){
+
+  }
+
+}
